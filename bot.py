87a3eba07166377cc901c62ee3298ab07d19d0c4
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-01 12:22:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types.web_app_info import WebAppInfo
 
-API_ID = 20786693
-API_HASH = 6eebbb7d9f9825a2d200c034bfbb7102
-BOT_TOKEN = 5927951494:AAEJwrPpR0n8Zhiqwn7L0YtPaIhdpewxo6A
+API_ID = "20786693"
+API_HASH = "6eebbb7d9f9825a2d200c034bfbb7102"
+BOT_TOKEN = "5927951494:AAEJwrPpR0n8Zhiqwn7L0YtPaIhdpewxo6A"
 
 bot = Client(
    API_ID,
@@ -16,8 +16,13 @@ bot = Client(
 async def Start(client, message):
    await message.reply(
      "TEST",
-     reply_markup=web_app=WebAppInfo(
+     reply_markup=InlineKeyboardButton(
+        web_app=WebAppInfo(
         "TEXT",
         url="https://google.com"
+        )
      )
    )
+
+print("START SUKSES")
+bot.start
