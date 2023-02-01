4024dce8cce1b72f5f5db6b79e13136f7e8f08e9
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-01 12:27:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,13 @@ from aiogram.types.web_app_info import WebAppInfo
 API_ID = "20786693"
 API_HASH = "6eebbb7d9f9825a2d200c034bfbb7102"
 BOT_TOKEN = "5927951494:AAEJwrPpR0n8Zhiqwn7L0YtPaIhdpewxo6A"
+SESSION = "BQCfRp489zu0XuFR6WePVPg10KQNaeDaic6Ns9Vy5M75nWSjct4CdZ8HDQAGHWBJX1X6l0uedCVxW5fneq3kAP1P3Xbk4GgtQM4X86Z61bsXVKDeAOpI3-wamMU8wEW1btgujfaIj0YVHwOc0FvB4g7xBNDyKBKkDQN7Ll7UUe9zQu_fSKwJylMxDlUGyxQbQySKM_STOyBFctF-nfmxqlXsF_of8omkQ7pgJzOjPDSi_IAsu7DeHp7RNinqkVf_ZtYkDh2A62xfxjSGC8HyO8_mZiUxKKT50m7XxPngQrCw6Wwg40HCk9LrQuFUQazBk01T08wgGnsRvYVShWiEiVW6f931YgA"
 
 bot = Client(
    api_id=API_ID,
    api_hash=API_HASH,
    bot_token=BOT_TOKEN,
+   session_name=SESSION,
 )
 
 @Client.on_message(filters.command("start"))
