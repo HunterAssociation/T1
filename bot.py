4d55204cd57fd61ffb1aa12e89b3d0300ac154f3
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-01 12:25:29 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@ from pyrogram import Client, filters
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types.web_app_info import WebAppInfo
 
-API_ID = int("20786693")
+API_ID = "20786693"
 API_HASH = "6eebbb7d9f9825a2d200c034bfbb7102"
 BOT_TOKEN = "5927951494:AAEJwrPpR0n8Zhiqwn7L0YtPaIhdpewxo6A"
 
 bot = Client(
-   API_ID,
-   API_HASH,
-   bot_token=BOT_TOKEN
+   api_id=API_ID,
+   api_hash=API_HASH,
+   bot_token=BOT_TOKEN,
 )
 
 @Client.on_message(filters.command("start"))
