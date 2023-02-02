fae66ec33862457a239c0ea03979d3ffc640fdbd
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-02 21:39:33 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import pyrogram
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 API_ID = "20786693"
 API_HASH = "6eebbb7d9f9825a2d200c034bfbb7102"
