db812c7c4cd242e23d17a64cafac925ab108a4da
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-03 01:58:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,10 +30,6 @@ async def start(Client, m:Message):
 if __name__ == "__main__":
     try:
         app.start()
-    except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("Your API_ID/API_HASH is not valid.")
-    except AccessTokenInvalid:
-        raise Exception("Your BOT_TOKEN is not valid.")
     uname = app.get_me().username
     print(f"@{uname} Botnya Sukses Mas Bay :*")
     idle()
