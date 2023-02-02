1a88d382db46c584a6293ce1e306f7bb0f2eb94f
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-02 21:43:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,5 +29,15 @@ async def start(Client, m:Message):
     )
 
 
-print("Bot Started")
-app.start()
+if __name__ == "__main__":
+    try:
+        app.start()
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your BOT_TOKEN is not valid.")
+    uname = app.get_me().username
+    print(f"@{uname} Botnya Sukses Mas Bay :*")
+    idle()
+    app.stop()
+    print("Botnya Berhenti Mas Bay :(")
