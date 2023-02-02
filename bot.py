e04ac6f93b0ed6fdde2824272543ca43a80d8340
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-02 21:38:55 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,33 @@
-import config
-import logging
-from pyrogram import Client, idle
-from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
-logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
+API_ID = "20786693"
+API_HASH = "6eebbb7d9f9825a2d200c034bfbb7102"
+BOT_TOKEN = "5999247031:AAG4i_PpP2x_Pcm6ZwDQCPYpDE6XJ7ugrYo"
 
 app = Client(
-    ":memory:",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-    session_string =config.SESSION,
-    plugins=dict(root="Test"),
+    "T1",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
 )
 
 
-# Run Bot
-if __name__ == "__main__":
-    try:
-        app.start()
-    except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("Your API_ID/API_HASH is not valid.")
-    except AccessTokenInvalid:
-        raise Exception("Your BOT_TOKEN is not valid.")
-    uname = app.get_me().username
-    print(f"@{uname} Botnya Sukses Mas Bay :*")
-    idle()
-    app.stop()
-    print("Botnya Berhenti Mas Bay :(")
+@Client.on_message(filters.command("start") & filters.private)
+async def start(Client, m:Message):
+    await m.reply(
+        text="Halo",
+        reply_markup=InlineKeyboardMarkup(
+            InlineKeyboardButton(
+                text="HALO TEST",
+                web_app=pyrogram.types.WebAppInfo(
+                    url="https://google.com/"
+                )
+            )
+        )
+    )
+
+
+
+app.start()
