5d6333f2815c755889c5173eb559b7b20b218326
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-03 09:10:13 +0700

    Update inline.py

--- asset/inline.py
+++ asset/inline.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 from aiogram.types.web_app_info import WebAppInfo
 
 
-ISTART_1 = InlineKeyboardMarkup().add(
+I_START = InlineKeyboardMarkup().add(
    InlineKeyboardButton(
       text="Halo",
       web_app=WebAppInfo(url="https://google.com")
