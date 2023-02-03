11424dd26d61017a097a6b212ff1dd14c4c95b5e
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-03 13:57:25 +0700

    Update inline.py

--- asset/inline.py
+++ asset/inline.py
@@ -3,7 +3,5 @@ from aiogram.types.web_app_info import WebAppInfo
 
 
 I_START = InlineKeyboardMarkup().add(
-   [InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/NekopoiSupport")],
-   [InlineKeyboardButton(text="ʀᴇᴘᴏʀᴛ", url="https://t.me/NekopoiSupport"),
-    InlineKeyboardButton(text="ᴅᴏɴᴀᴛᴇ", web_app=WebAppInfo(url="https://telegra.ph/DONATE-ME-02-03"))]
+    InlineKeyboardButton(text="ᴅᴏɴᴀᴛᴇ", web_app=WebAppInfo(url="https://telegra.ph/DONATE-ME-02-03"))
 )
