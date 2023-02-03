f7863d1849cd8752edd86d33ce31c98c7bd46c7a
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-03 08:17:39 +0700

    Update start.py

--- asset/inline/start.py
+++ asset/inline/start.py
@@ -1,3 +1,12 @@
-START = f"""
-   WELCOME TO MOBILE LEGENDS
-"""
+from aiogram.types import
+     InlineKeyboardMarkup,
+     InlineKeyboardButton,
+
+
+
+ISTART_1 = InlineKeyboardMarkup().add(
+   InlineKeyboardButton(
+      text="Halo",
+      web_app=WebAppInfo(url="https://google.com")
+   )
+)
