0493b0649a1a36b65f16b17d31ee6fb4c9360d7a
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-03 20:21:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,15 +5,14 @@ from aiogram.types.web_app_info import WebAppInfo
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from asset import message, inline
-
 from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
 app = Dispatcher(bot)
 
-
-
-
+###########################################
+#                S T A R T                #
+###########################################
 @app.message_handler(commands="start")
 async def start(m:Message):
     user_id = m.from_user.id
@@ -21,12 +20,27 @@ async def start(m:Message):
 
     START1 = await bot.send_message(m.chat.id, text="➜ 𝙱𝚎𝚛𝚊𝚗𝚍𝚊", reply_markup=inline.K_START)
     START2 = await bot.send_photo(m.chat.id, photo="https://telegra.ph/file/ef7261e2a4bec533ec771.jpg", caption=f"<b>Hai: <a href='tg://user?id={user_id}'>{user_name}</a>\nSelamat datang di NekoPoiBot. \n\nFitur Bot:</b> \n➥ No Iklan.\n➥ Akses Sangat Mudah.\n➥ Bebas Streaming & Download.\n\n\n✥ <b>Total Users ⋙</b> Users", parse_mode='html', reply_markup=inline.I_START)    
+
     await asyncio.sleep(300)
     await m.delete()
     await START1.delete()
     await START2.delete()
 
+###########################################
+#              B E R A N D A              #
+###########################################
+@app.message_handler(commands="𝗕𝗘𝗥𝗔𝗡𝗗𝗔", )
+async def start(m:Message):
+    user_id = m.from_user.id
+    user_name = m.from_user.first_name
 
+    BERANDA1 = await bot.send_message(m.chat.id, text="➜ 𝙱𝚎𝚛𝚊𝚗𝚍𝚊", reply_markup=inline.K_START)
+    BERANDA2 = await bot.send_photo(m.chat.id, photo="https://telegra.ph/file/ef7261e2a4bec533ec771.jpg", caption=f"<b>Hai: <a href='tg://user?id={user_id}'>{user_name}</a>\nSelamat datang di NekoPoiBot. \n\nFitur Bot:</b> \n➥ No Iklan.\n➥ Akses Sangat Mudah.\n➥ Bebas Streaming & Download.\n\n\n✥ <b>Total Users ⋙</b> Users", parse_mode='html', reply_markup=inline.I_START)    
+
+    await asyncio.sleep(300)
+    await m.delete()
+    await BERANDA1.delete()
+    await BERANDA2.delete()
     
 print("Bot Started")
 if __name__ == '__main__':
