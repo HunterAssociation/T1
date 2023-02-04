7dd4994381811eaf7b3f3e913c92daa5d04454f0
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-04 16:08:47 +0700

    Update help.py

--- Nekobot/help.py
+++ Nekobot/help.py
@@ -3,6 +3,6 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-@pyro.on_message(filters.command("help"))
+@Client.on_message(filters.command("help"))
 async def Help(bot:Client, m:Message):
    await m.reply("Halo")
