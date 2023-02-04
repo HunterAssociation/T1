92c5d18744369e57d88d36592a204428b45b52da
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-04 19:11:39 +0700

    Update tes.py

--- Nekobot/tes.py
+++ Nekobot/tes.py
@@ -1,6 +1,6 @@
 from aiogram.types import Message
 from bot import aio
 
-@aio.message_handler(commands=["tes"])
+@aio.message_handler(lambda message : message.text == 'tes',state='*')
 async def tesdoang(m:Message):
    await m.reply("Hallo")
