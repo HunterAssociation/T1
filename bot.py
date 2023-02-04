1f8025191771305d2829a623e4e418e746911be4
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-04 16:00:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from pyrogram import Client, idle
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
@@ -18,8 +20,18 @@ async def start(m:Message):
     user_id = m.from_user.id
     user_name = m.from_user.first_name
 
-    START1 = await bot.send_message(m.chat.id, text="➜ 𝙱𝚎𝚛𝚊𝚗𝚍𝚊", reply_markup=inline.K_START)
-    START2 = await bot.send_photo(m.chat.id, photo="https://telegra.ph/file/ef7261e2a4bec533ec771.jpg", caption=f"<b>Hai: <a href='tg://user?id={user_id}'>{user_name}</a>\nSelamat datang di NekoPoiBot. \n\nFitur Bot:</b> \n➥ No Iklan.\n➥ Akses Sangat Mudah.\n➥ Bebas Streaming & Download.\n\n\n✥ <b>Total Users ⋙</b> Users", parse_mode='html', reply_markup=inline.I_START)    
+    START1 = await bot.send_message(
+       m.chat.id,
+       text="➜ 𝙱𝚎𝚛𝚊𝚗𝚍𝚊",
+       reply_markup=inline.K_START
+    )
+    START2 = await bot.send_photo(
+       m.chat.id,
+       photo="https://telegra.ph/file/ef7261e2a4bec533ec771.jpg",
+       caption=f"<b>Hai: <a href='tg://user?id={user_id}'>{user_name}</a>\nSelamat datang di NekoPoiBot. \n\nFitur Bot:</b> \n➥ No Iklan.\n➥ Akses Sangat Mudah.\n➥ Bebas Streaming & Download.\n\n\n✥ <b>Total Users ⋙</b> Users",
+       parse_mode='html',
+       reply_markup=inline.I_START
+    )    
 
     await asyncio.sleep(300)
     await m.delete()
@@ -57,6 +69,5 @@ if __name__ == '__main__':
     executor.start_polling(aio)
     print("PYRO Started")
     pyro.start()
-
     await idle()
     pyro.stop()
