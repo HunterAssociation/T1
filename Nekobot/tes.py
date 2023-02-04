051f830c4933eed20c4ed005bc78e846897cc870
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-04 19:12:39 +0700

    Delete tes.py

--- Nekobot/tes.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import Message
-from bot import aio
-
-@aio.message_handler(lambda message : message.text == 'tes',state='*')
-async def tesdoang(m:Message):
-   await m.reply("Hallo")
