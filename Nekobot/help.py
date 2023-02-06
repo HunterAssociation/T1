61c1d38bea682b15a383dc8f259935d1ac1a7498
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-06 19:45:19 +0700

    Delete help.py

--- Nekobot/help.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-
-@Client.on_message(filters.command("help"))
-async def Help(bot:Client, m:Message):
-   await m.reply("Halo")
